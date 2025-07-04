eed8856a91f25ece05a84bf6617eef3df49f1f88
HerosGuimaraes12 <kiosqink@gmail.com>
2025-07-04 14:53:05 -0300

    Create maior-idade.py

---
+++ maior-idade.py
@@ -0,0 +1,13 @@
+# Nível 1 — Revisão + lógica simples
+# 2. Maior ou menor de idade?
+# Peça ao usuário sua idade e diga:
+# - "Você é maior de idade." se tiver 18 anos ou mais
+# - "Você é menor de idade." se tiver menos de 18
+
+# Exemplo de código:
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
